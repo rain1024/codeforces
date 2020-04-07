d5184b1c03acf8fa1aa0c388e6b298c0fb77e31b
anhv <anhv.ict91@gmail.com>
2020-04-07 08:01:34 +0700

    add problem 69A

---
+++ problems/69/69A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void solve(){
+
+}
+int main() {
+   int n;
+   int x, y, z;
+   int sx = 0, sy = 0, sz = 0;
+   cin >> n;
+   for(int i = 0; i < n; i++){
+       cin >> x;
+       cin >> y;
+       cin >> z;
+       sx += x;
+       sy += y;
+       sz += z;
+   }
+   if(sx == 0 & sy == 0 & sz == 0){
+       cout << "YES";
+   } else {
+       cout << "NO";
+   }
+}
